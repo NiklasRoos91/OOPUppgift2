7097510eb3c23d4269fb9bc939be51b625cff124
NiklasRoos91 <niklas_roos@live.se>
2024-09-19 18:57:09 +0200

    La till klassen Member med två metoder

---
+++ Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Member : User
+    {
+        public override void BorrowItem()
+        {
+            Console.WriteLine("Medlem lånar");
+        }
+
+        public override void ReturnItem()
+        {
+            Console.WriteLine("Medlem lämnar Tillbaka");
+        }
+    }
+}
